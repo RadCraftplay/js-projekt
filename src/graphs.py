0a33228dbf9b2be7940c3b641cf24e0a4c84d242
RadCraftplay <radcraftplay2@gmail.com>
2021-06-09 19:00:32 +0200

    Added ListDefinedGraph

--- src/graphs.py
+++ src/graphs.py
@@ -60,6 +60,38 @@ class MatrixDefinedGraph(AbstractGraph):
         return [i for i in range(len(self._incidence_matrix)) if self._incidence_matrix[i][edge] == 1]
 
 
+class ListDefinedGraph(AbstractGraph):
+    def __init__(self, list_of_adjacency_lists):
+        self._incidental_edges = self.to_incidental_edge_list(list_of_adjacency_lists)
+
+    @staticmethod
+    def to_incidental_edge_list(list_of_adjacency_lists):
+        edges = []
+        for index, adjacency_list in enumerate(list_of_adjacency_lists):
+            for node in adjacency_list:
+                if edges.__contains__((node, index)):
+                    continue
+                edges.append((index, node))
+
+        return edges
+
+    def get_node_list(self):
+        nodes = utils.flatmap(self._incidental_edges)
+        max_val = 0 if nodes == [] else max(nodes) + 1
+        return [i for i in range(max_val)]
+
+    def get_incidental_edges(self, node):
+        incidental_edges_to_node = []
+        for index, edge in enumerate(self._incidental_edges):
+            if node in edge:
+                incidental_edges_to_node.append(index)
+
+        return incidental_edges_to_node
+
+    def get_nodes_connected_by_edge(self, edge):
+        return self._incidental_edges[edge]
+
+
 class GraphExplorer(object):
     def __init__(self, graph):
         if graph is not AbstractGraph:
--- test/graphtests.py
+++ test/graphtests.py
@@ -43,10 +43,45 @@ class MatrixDefinedGraphTests(unittest.TestCase):
         self.assertSequenceEqual(neighbours_2, [0, 1])
 
 
+class ListDefinedGraphTests(unittest.TestCase):
+    def setUp(self):
+        self.test_list = [[2], [2], [0, 1]]
+
+    def test_get_node_list_should_return_valid_nodes(self):
+        graph = graphs.ListDefinedGraph(self.test_list)
+        self.assertSequenceEqual(graph.get_node_list(), [0, 1, 2])
+
+    def test_get_incidental_edges_returns_proper_list(self):
+        graph = graphs.ListDefinedGraph(self.test_list)
+        edges_0 = graph.get_incidental_edges(0)
+        edges_1 = graph.get_incidental_edges(1)
+        edges_2 = graph.get_incidental_edges(2)
+        self.assertSequenceEqual(edges_0, [0])
+        self.assertSequenceEqual(edges_1, [1])
+        self.assertSequenceEqual(edges_2, [0, 1])
+
+    def test_get_nodes_connected_by_edge_returns_proper_sequence(self):
+        graph = graphs.ListDefinedGraph(self.test_list)
+        nodes_0 = graph.get_nodes_connected_by_edge(0)
+        nodes_1 = graph.get_nodes_connected_by_edge(1)
+        self.assertSequenceEqual(nodes_0, [0, 2])
+        self.assertSequenceEqual(nodes_1, [1, 2])
+
+    def test_get_neighbour_nodes_returns_proper_nodes(self):
+        graph = graphs.ListDefinedGraph(self.test_list)
+        neighbours_0 = graph.get_neighbour_nodes(0)
+        neighbours_1 = graph.get_neighbour_nodes(1)
+        neighbours_2 = graph.get_neighbour_nodes(2)
+        self.assertSequenceEqual(neighbours_0, [2])
+        self.assertSequenceEqual(neighbours_1, [2])
+        self.assertSequenceEqual(neighbours_2, [0, 1])
+
+
 if __name__ == '__main__':
     def get_test_suite():
         s = unittest.TestSuite()
         s.addTest(MatrixDefinedGraphTests("matrix_defined_graph_tests"))
+        s.addTest(ListDefinedGraphTests("list_defined_graph_tests"))
         return s
 
     runner = unittest.TextTestRunner()
