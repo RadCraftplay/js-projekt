1cf34db3db16923e1f462aa39b5271b52c3bf11e
RadCraftplay <radcraftplay2@gmail.com>
2021-06-09 17:36:11 +0200

    Fixed flatmap method

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ def flatmap(array):
     ret_val = []
 
     for top_element in array:
-        for lower_element in array:
+        for lower_element in top_element:
             ret_val.append(lower_element)
 
     return ret_val
