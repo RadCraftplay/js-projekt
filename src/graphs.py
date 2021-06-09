4de6e5ed5c67bfce968a3303fd018f2785d6c779
RadCraftplay <radcraftplay2@gmail.com>
2021-06-09 21:28:31 +0200

    Changed Queue to dequeue

--- src/graphs.py
+++ src/graphs.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from collections import deque
 from src import utils
 
 
@@ -108,19 +108,19 @@ class BfsExplorer(GraphExplorer):
         return self._reconstruct_path(starting_node, last_node, previous_nodes)
 
     def _solve(self, starting_node):
-        node_queue = Queue()
-        node_queue.put(starting_node)
+        node_queue = deque()
+        node_queue.append(starting_node)
 
-        visited = [False if i != starting_node else True for i in range(self._graph.get_node_list())]
-        previous_nodes = [None for i in range(self._graph.get_node_list())]
+        visited = [False if i != starting_node else True for i in self._graph.get_node_list()]
+        previous_nodes = [None for _ in self._graph.get_node_list()]
 
-        while node_queue.not_empty:
-            current_node = node_queue.get()
+        while len(node_queue):
+            current_node = node_queue.popleft()
             neighbours = self._graph.get_neighbour_nodes(current_node)
 
             for next_node in neighbours:
                 if not visited[next_node]:
-                    node_queue.put(next_node)
+                    node_queue.append(next_node)
                     visited[next_node] = True
                     previous_nodes[next_node] = current_node
         return previous_nodes
