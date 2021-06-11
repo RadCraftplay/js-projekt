6b3eea5632e5b2010b2a5da59881c1ceaf949aa7
RadCraftplay <radcraftplay2@gmail.com>
2021-06-11 13:30:58 +0200

    Added more tests

--- test/explorerstests.py
+++ test/explorerstests.py
@@ -6,8 +6,39 @@ from src import graphs, explorers, data
 class BfsExplorerTests(unittest.TestCase):
     def setUp(self):
         self.test_proper_matrix = [[0, 0, 1], [0, 0, 1], [1, 1, 0]]
+        self.test_proper_list = [[2], [2], [0, 1]]
 
-    def test_find_path_matrix(self):
+    def test_find_path_direct_matrix(self):
+        """
+        Tests if BfsExplorer can find direct path
+        (uses matrix representation)
+        """
+        test_matrix_big = [[0, 1, 1, 0, 0],
+                           [1, 0, 0, 1, 0],
+                           [1, 0, 0, 0, 1],
+                           [0, 1, 0, 0, 1],
+                           [0, 0, 1, 1, 0]]
+        test_graph = graphs.MatrixDefinedGraph(test_matrix_big)
+        explorer = explorers.BfsExplorer(test_graph)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [0, 1])
+
+    def test_find_path_direct_list(self):
+        """
+        Tests if BfsExplorer can find direct path
+        (uses list representation)
+        """
+        test_list_big = [[2, 1],
+                         [3, 0],
+                         [0, 4],
+                         [1, 4],
+                         [2, 3]]
+        test_graph = graphs.ListDefinedGraph(test_list_big)
+        explorer = explorers.BfsExplorer(test_graph)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [0, 1])
+
+    def test_find_path_1stop_matrix(self):
         """
         Tests if BfsExplorer can find path if on the route
         there is at least one stop
@@ -18,20 +49,82 @@ class BfsExplorerTests(unittest.TestCase):
         path = explorer.find_path(0, 1)
         self.assertSequenceEqual(path, [0, 2, 1])
 
-    def test_should_not_find_path(self):
+    def test_find_path_1stop_list(self):
+        """
+        Tests if BfsExplorer can find path if on the route
+        there is at least one stop
+        (uses list representation)
+        """
+        test_list = graphs.ListDefinedGraph(self.test_proper_list)
+        explorer = explorers.BfsExplorer(test_list)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [0, 2, 1])
+
+    def test_find_path_3stops_matrix(self):
+        """
+        Tests if BfsExplorer can find path if on the route
+        there is at least three stops
+        (uses matrix representation of a graph)
+        """
+        test_matrix_big = [[0, 0, 1, 0, 0],
+                           [0, 0, 0, 1, 0],
+                           [1, 0, 0, 0, 1],
+                           [0, 1, 0, 0, 1],
+                           [0, 0, 1, 1, 0]]
+        test_graph = graphs.MatrixDefinedGraph(test_matrix_big)
+        explorer = explorers.BfsExplorer(test_graph)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [0, 2, 4, 3, 1])
+
+    def test_find_path_3stops_list(self):
+        """
+        Tests if BfsExplorer can find path if on the route
+        there is at least three stops
+        (uses list representation of a graph)
+        """
+        test_list_big = [[2],
+                         [3],
+                         [0, 4],
+                         [1, 4],
+                         [2, 3]]
+        test_graph = graphs.ListDefinedGraph(test_list_big)
+        explorer = explorers.BfsExplorer(test_graph)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [0, 2, 4, 3, 1])
+
+    def test_should_not_find_path_matrix(self):
         """
         Tests if BfsExplorer can't find path
         if there are no connections to destination city
         """
-        test_matrix = [[0, 0, 1], [0, 0, 0], [1, 0, 0]]
-        test_graph = graphs.MatrixDefinedGraph(test_matrix)
+        test_matrix_big = [[0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 1],
+                           [0, 0, 0, 0, 1],
+                           [0, 0, 1, 1, 0]]
+        test_graph = graphs.MatrixDefinedGraph(test_matrix_big)
+        explorer = explorers.BfsExplorer(test_graph)
+        path = explorer.find_path(0, 1)
+        self.assertSequenceEqual(path, [])
+
+    def test_should_not_find_path_list(self):
+        """
+        Tests if BfsExplorer can't find path
+        if there are no connections to destination city
+        """
+        test_list_big = [[],
+                         [],
+                         [4],
+                         [4],
+                         [2, 3]]
+        test_graph = graphs.ListDefinedGraph(test_list_big)
         explorer = explorers.BfsExplorer(test_graph)
         path = explorer.find_path(0, 1)
         self.assertSequenceEqual(path, [])
 
     def test_find_path_cities(self):
         """
-        Same as test_find_path_matrix, uses
+        Same as test_find_path_1stop_matrix, uses
         city names instead of node ids
         """
         test_graph = graphs.MatrixDefinedGraph(self.test_proper_matrix)
